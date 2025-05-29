7cd260ec0b3f27789347e7c82c840247da8e583d
Marisixande <mariaclaramacedosouza71@gmail.com>
2025-05-28 22:09:28 -0300

    atividade do dia 28/05/2025 para dia 04/06/2025

---
+++ idade-cahorro.js
@@ -0,0 +1,10 @@
+let idade = prompt("Insira a idade do se cachorro para descobrir se ele se aposentaria em parametros humanos:")
+if ((idade*7)>65){
+   alert("Seu cachorro ja poderia se aposentar!")
+}
+else if((idade*7<1)){
+    alert("Seu cachorro e um filhote!")
+}
+else{
+     alert("Seu cachorro nao poderia se aposentar ainda!")
+}
