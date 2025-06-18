f0866f4eebe90ab89860631e244e410adce2da9c
Marisixande <mariaclaramacedosouza71@gmail.com>
2025-06-18 17:14:40 -0300

    Add files via upload

--- peso.js
+++ peso.js
@@ -4,6 +4,23 @@ let sexo = Number (prompt ("Digite seu sexo(Sendo 1 para feminino e 2 para mascu
 let pesofem = ((62.1 * altura) - 44.7)
 let pesomasc = (72.7 * altura) 
 
-if(sexo === 1){
-    pesofem
+switch (sexo) {
+    case 1 :
+        if(pesofem != 44.7){
+            alert("Voce nao esta no peso ideal")
+        } else {
+            alert("Voce esta no peso ideal!")
+        }
+        break;
+     case 2 :
+        if(pesomasc = 58){
+                alert("Voce esta no peso ideal!")
+            }
+        else if(pesomasc != 58) {
+                alert("Voce nao esta no peso ideal!")
+        }
+        break;
+     default:
+        alert("Voce nao existe")
+
 }
